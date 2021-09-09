4560a832df267942f90b80ca42439c11095f0509
Ryan Murphy <ryanmurphy@gatech.edu>
2021-09-09 18:47:47 -0400

    Updates

--- src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-export const trackSort = track => (a, b) => toMs(a[track]) - toMs(b[track]);
-
-export function toMs(duration) {
-  let arr = duration.split(/[:.]/);
-  return arr[0] * 60 * 1000 + arr[1] * 1000 + arr[2];
-}
