0ed77141d261dc6bdd66343553ac5908cf2067bc
Ryan Murphy <ryanmurphy@gatech.edu>
2021-09-12 11:22:56 -0400

    filter profile by season

--- src/views/Player.js
+++ src/views/Player.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { Row, Col, Card } from "react-bootstrap";
 import tracks from "../data/maps.json";
-import {subtractDuration} from "../utils/utils";
+import { subtractDuration } from "../utils/utils";
 
 function Player(props) {
 
   const { player } = useParams();
+  const [season, setSeason] = useState(0);
 
-  let driverGames = props.records.filter(record => record.driver === player && record.result !== "desync");
+  let records = parseInt(season) === 0 ? props.records : props.records.filter(record => record.season === season);
+
+  let driverGames = records.filter(record => record.driver === player && record.result !== "desync");
   let driver = driverGames.reduce((sum, record) => sum + (record.result === "win" ? 1 : 0), 0);
-  let itemsGames = props.records.filter(record => record.items === player && record.result !== "desync");
+  let itemsGames = records.filter(record => record.items === player && record.result !== "desync");
   let items = itemsGames.reduce((sum, record) => sum + (record.result === "win" ? 1 : 0), 0);
 
   let stats = {};
@@ -20,28 +23,28 @@ function Player(props) {
   let driverTrackWins = 0;
   let itemTrackWins = 0;
   // TODO: change to `let stats = Object.values(tracks).flat().map(...)
-  props.records.length && Object.values(tracks).flat().forEach(track => {
+  records.length && Object.values(tracks).flat().forEach(track => {
     let driverWins = 0;
     let itemWins = 0;
     let driverGames = 0;
     let itemGames = 0;
     let fastestDriver = {};
-    for (let i = 0; i < props.records.length / 2; i++) {
-      if (props.records[2 * i][track] === "desync") continue;
-      if (props.records[2 * i].driver === player) {
+    for (let i = 0; i < records.length / 2; i++) {
+      if (records[2 * i][track] === "desync") continue;
+      if (records[2 * i].driver === player) {
         driverGames++;
-        if (fastestDriver[track] === undefined || props.records[2 * i][track] < fastestDriver[track]) fastestDriver = props.records[2 * i];
-        if (props.records[2 * i][track] < props.records[2 * i + 1][track]) driverWins++;
-      } else if (props.records[2 * i].items === player) {
+        if (fastestDriver[track] === undefined || records[2 * i][track] < fastestDriver[track]) fastestDriver = records[2 * i];
+        if (records[2 * i][track] < records[2 * i + 1][track]) driverWins++;
+      } else if (records[2 * i].items === player) {
         itemGames++;
-        if (props.records[2 * i][track] < props.records[2 * i + 1][track]) itemWins++;
-      } else if (props.records[2 * i + 1].driver === player) {
+        if (records[2 * i][track] < records[2 * i + 1][track]) itemWins++;
+      } else if (records[2 * i + 1].driver === player) {
         driverGames++;
-        if (fastestDriver[track] === undefined || props.records[2 * i + 1][track] < fastestDriver[track]) fastestDriver = props.records[2 * i + 1];
-        if (props.records[2 * i][track] > props.records[2 * i + 1][track]) driverWins++;
-      } else if (props.records[2 * i + 1].items === player) {
+        if (fastestDriver[track] === undefined || records[2 * i + 1][track] < fastestDriver[track]) fastestDriver = records[2 * i + 1];
+        if (records[2 * i][track] > records[2 * i + 1][track]) driverWins++;
+      } else if (records[2 * i + 1].items === player) {
         itemGames++;
-        if (props.records[2 * i][track] > props.records[2 * i + 1][track]) itemWins++;
+        if (records[2 * i][track] > records[2 * i + 1][track]) itemWins++;
       }
     }
     let record = props.records.map(r => r[track]).reduce((prev, cur) => cur < prev ? cur : prev);
@@ -60,6 +63,10 @@ function Player(props) {
   return (
     <Container>
       <h3>{player}</h3>
+      <select value={season} onChange={e => setSeason(e.target.value)}>
+        <option value={0}>All Seasons</option>
+        {[...new Set(props.records.map(r => r.season))].map(season => <option key={season} value={season}>Season {season}</option>)}
+      </select>
       <Row>
         <Col md={4}>
           Runback Stats
@@ -97,7 +104,7 @@ function Player(props) {
                 </tr>
               </thead>
               <tbody>
-                {props.records.length ? Object.values(tracks).flat().map(track => {
+                {records.length ? Object.values(tracks).flat().map(track => {
                   return (
                     <tr key={track}>
                       <td>{track}</td>
