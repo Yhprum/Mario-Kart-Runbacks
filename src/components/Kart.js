ba173958c6dc3a8a25008926039245000798926c
Ryan Murphy <ryanmurphy@gatech.edu>
2021-11-01 12:47:38 -0400

    new kart

--- src/components/Kart.js
+++ src/components/Kart.js
@@ -6,9 +6,10 @@ import green from "../img/karts/green.png";
 import waluigi from "../img/karts/waluigi.png";
 import bullet from "../img/karts/bullet.png";
 import daisy from "../img/karts/daisy.png";
+import boo from "../img/karts/boo.png";
 
 function Kart({ kart }) {
-  let images = { train, blue, pink, green, waluigi, bullet, daisy };
+  let images = { train, blue, pink, green, waluigi, bullet, daisy, boo };
   return (
     <img src={images[kart]} alt={kart} className="kart-icon"/>
   );
