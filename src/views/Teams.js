931a7a8318b375563161d118d9a5da3aa9e45922
Yhprum <ryanmurphy@gatech.edu>
2021-11-13 17:52:43 -0500

    more teams stats

--- src/views/Teams.js
+++ src/views/Teams.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { Row, Col, Table } from "react-bootstrap";
-import { Link, withRouter } from "react-router-dom";
-import { players, teams, toPercent } from "../utils/utils";
-import Winrate from "../components/Winrate";
-import Kart from "../components/Kart";
+import { Table } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
+import { teams, toMs, toPercent } from "../utils/utils";
+import maps from "../data/maps.json";
 
 function Teams({ stats, records }) {
 
   let teamRecords = {};
-  if (records) teams.forEach(team => {
-    let games = records.filter(record => record.driver === team.driver && record.items === team.items && record.result !== "desync");
-    teamRecords[Object.values(team)] = {
-      games: games.length,
-      wins: games.reduce((sum, record) => sum + (record.result === "win" ? 1 : 0), 0)
-    }
-  });
+  if (records.length) {
+    let tracks = Object.values(maps).flat();
+    let trackRecords = tracks.map(track => records.reduce((prev, cur) => cur[track] < prev[track] ? cur : prev));
+
+    teams.forEach(team => {
+      let games = records.filter(record => record.driver === team.driver && record.items === team.items && record.result !== "desync");
+      teamRecords[Object.values(team)] = {
+        games: games.length,
+        wins: games.reduce((sum, record) => sum + (record.result === "win" ? 1 : 0), 0),
+        fastest: games.length ? games.reduce((fastest, cur) => toMs(cur.time) < toMs(fastest.time) ? cur : fastest) : 0,
+        records: trackRecords.reduce((sum, record) => sum + (record.driver === team.driver && record.items === team.items ? 1 : 0), 0)
+      }
+    });
+    teams.sort((a, b) => (teamRecords[Object.values(b)].wins / teamRecords[Object.values(b)].games) - (teamRecords[Object.values(a)].wins / teamRecords[Object.values(a)].games));
+  }
 
   return (
     <div className="container">
@@ -25,20 +32,24 @@ function Teams({ stats, records }) {
           <th>Driver</th>
           <th>Items</th>
           <th>Occurrences</th>
+          <th>Fastest Time</th>
+          <th>Number of Records</th>
           <th>Runbacks</th>
           <th>Tracks</th>
         </tr>
         </thead>
         <tbody>
-        {teams.map(team => [
+        {records.length ? teams.map(team =>
           <tr key={team.driver + team.items}>
             <td>{team.driver}</td>
             <td>{team.items}</td>
-            <td>{records.length ? teamRecords[Object.values(team)].games : 0}</td>
-            <td>{records.length ? toPercent(teamRecords[Object.values(team)].wins / teamRecords[Object.values(team)].games) : "0%"}</td>
+            <td>{teamRecords[Object.values(team)].games}</td>
+            <td>{teamRecords[Object.values(team)].fastest.time}</td>
+            <td>{teamRecords[Object.values(team)].records}</td>
+            <td>{toPercent(teamRecords[Object.values(team)].wins / teamRecords[Object.values(team)].games)}</td>
             <td>{toPercent(stats[Object.values(team)].wins / stats[Object.values(team)].tracks)}</td>
           </tr>
-        ])}
+        ) : null}
         </tbody>
       </Table>
     </div>
