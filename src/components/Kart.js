973d1d0823e16412ae390cf0fbdae5e374221434
Ryan Murphy <ryanmurphy@gatech.edu>
2021-12-01 12:33:59 -0500

    season 6 runbacks

--- src/components/Kart.js
+++ src/components/Kart.js
@@ -8,9 +8,10 @@ import bullet from "../img/karts/bullet.png";
 import daisy from "../img/karts/daisy.png";
 import boo from "../img/karts/boo.png";
 import luigi from "../img/karts/luigi.png";
+import mario from "../img/karts/mario.png";
 
 function Kart({ kart }) {
-  let images = { train, blue, pink, green, waluigi, bullet, daisy, boo, luigi };
+  let images = { train, blue, pink, green, waluigi, bullet, daisy, boo, luigi, mario };
   return (
     <img src={images[kart]} alt={kart} className="kart-icon"/>
   );
